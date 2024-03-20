f906eaf3be744089e838ec2763ef144b350b71e5
ItsMeNoobieboy <54125080+ItsMeNoobieboy@users.noreply.github.com>
2024-03-19 21:20:55 -0400

    Update domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://yanchengzhao.com",
+  site: "https://yanchengzhao.online",
   integrations: [tailwind(), svelte()]
 });
