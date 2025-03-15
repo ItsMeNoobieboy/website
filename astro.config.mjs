cf4bc786570322f11dec9130f408bc3c1e8ff216
ItsMeNoobieboy <54125080+ItsMeNoobieboy@users.noreply.github.com>
2025-03-15 17:06:44 -0400

    Update url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://yanchengzhao.online",
+  site: "https://yanchengzhao.com",
   integrations: [tailwind()]
 });
