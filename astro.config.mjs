9ce1c285c014fb89dc0181d0c32946a1dcbf644a
ItsMeNoobieboy <54125080+ItsMeNoobieboy@users.noreply.github.com>
2026-07-08 13:42:16 +0800

    Update tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://yanchengzhao.com",
-  integrations: [tailwind()]
+  integrations: [],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
