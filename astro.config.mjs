6006016c39b3694a420f2acd620df65ca59bc035
ItsMeNoobieboy <54125080+ItsMeNoobieboy@users.noreply.github.com>
2026-07-10 15:38:17 +0800

    Add prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://yanchengzhao.com",
-  integrations: [],
+    site: "https://yanchengzhao.com",
+    integrations: [],
 
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    prefetch: {
+        prefetchAll: true, // prefetch every link, not just ones you tag
+        defaultStrategy: "hover", // start fetching when the cursor hovers/focuses a link
+    },
+
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
